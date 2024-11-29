a97d13160153eb3072218b1b9c796cebca39f193
Abdel Karim Metlej <abdelkarimmetlej1@gmail.com>
2024-11-29 22:40:08 +0200

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// App.tsx
+import React from "react";
+import Game from "./screens/Game/Game"; // Import the Game screen
 
-function App() {
+import "./App.css"; // Import custom CSS styles
+
+// Functional component for App
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game /> {/* Render the Game component */}
     </div>
   );
-}
+};
 
 export default App;
